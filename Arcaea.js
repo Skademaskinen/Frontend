a7fb44df356e0107878e02d98d9a90bce5224429
Mast3rwaf1z <thomasmollerjensen@live.dk>
2022-04-02 02:45:53 +0200

    some video improvements

---
+++ Arcaea.js
@@ -0,0 +1,34 @@
+var videos = ["https://www.youtube.com/embed/inc1ww8Sz0I", "https://www.youtube.com/embed/_7ZQtgx3PBw", "https://www.youtube.com/embed/3A5-TKMXHqE", "https://www.youtube.com/embed/5kLDeERwdHg"]
+document.getElementById("player").src = videos[0]
+current = 0
+function next() {
+    if (current != videos.length - 1) {
+        current = current + 1
+    }
+    else {
+        current = 0
+    }
+    document.getElementById("player").src = videos[current]
+}
+function previous() {
+    if (current != 0) {
+        current = current - 1
+    }
+    else {
+        current = videos.length - 1
+    }
+    document.getElementById("player").src = videos[current]
+}
+
+for (var i = 0; i < videos.length; i++) {
+    button = document.createElement("button")
+    console.log(videos[i])
+    button.onclick = event => {
+        var index = event.target.innerHTML.substring(7)
+        console.log(index)
+        document.getElementById("player").src = videos[index]
+        current = i
+    }
+    button.innerHTML = "video #" + i
+    document.getElementById("parent").appendChild(button)
+}
