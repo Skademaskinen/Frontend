8d68b0b0300c5da3bbcda694daaa4528e1ce2618
Mast3rwaf1z <thomasmollerjensen@live.dk>
2022-04-03 01:46:58 +0200

    made a very smart page :)

--- videos.js
+++ videos.js
@@ -1,9 +1,10 @@
 var videos = [
     ["https://www.youtube.com/embed/inc1ww8Sz0I", "https://www.youtube.com/embed/_7ZQtgx3PBw", "https://www.youtube.com/embed/3A5-TKMXHqE", "https://www.youtube.com/embed/5kLDeERwdHg"],
     ["https://youtube.com/embed/YA2GsBEoXiE", "https://youtube.com/embed/QaFckRerl-A", "https://youtube.com/embed/AE_EGtkLLRk", "https://youtube.com/embed/KEBb0DSb5tg", "https://youtube.com/embed/VLxAAO0jUuM", "https://youtube.com/embed/X_YVjT6vo7Q", "https://youtube.com/embed/W0zcKH5BfAo", "https://youtube.com/embed/ykEh-K4vkdw", "https://youtube.com/embed/nU-wVMtWOjA", "https://youtube.com/embed/eLDrdH0bWhY", "https://youtube.com/embed/VK_qqK7zs-8", "https://youtube.com/embed/mphA7fhATh8", "https://youtube.com/embed/189VChlnAK4",]]
-document.getElementById("player0").src = videos[0][0]
-document.getElementById("player1").src = videos[1][0]
-current = [0, 0]
+var current = []
+for (var i = 0; i < videos.length; i++) {
+    current[i] = 0
+}
 function next(index) {
     if (current[index] != videos[index].length - 1) {
         current[index] = current[index] + 1
@@ -22,20 +23,56 @@ function previous(index) {
     }
     document.getElementById("player" + index).src = videos[index][current[index]]
 }
-for (var i = 0; i < videos.length; i++) {
-    for (var j = 0; j < videos[i].length; j++) {
-        button = document.createElement("button")
-        console.log(videos[j])
-        button.id = "button" + i + j
-        button.onclick = event => {
-            var player_id = event.target.id.substring(6, 7)
-            var index = event.target.id.substring(7)
-            console.log(event.target.id)
-            console.log(player_id, index)
-            document.getElementById("player" + player_id).src = videos[player_id][index]
-            current[i] = index
+
+function gen_page() {
+    for (var i = 0; i < videos.length; i++) {
+        //create stuff there is only one of, ie prev, next buttons and video player
+        var master = document.getElementById("section" + i)
+        player_div = document.createElement("div")
+        player_div.style = "text-align: center"
+        master.appendChild(player_div)
+        player = document.createElement("iframe")
+        player.id = "player" + i
+        player.src = videos[i][current[i]]
+        player.width = "1280px"
+        player.height = "720px"
+        player.style = "text-align: center"
+        player.allow = "fullscreen"
+        player_div.appendChild(player)
+        btn_div = document.createElement("div")
+        btn_div.style = "text-align: center"
+        master.appendChild(btn_div)
+        prev_btn = document.createElement("button")
+        prev_btn.id = i
+        prev_btn.onclick = event => {
+            next(event.target.id)
         }
-        button.innerHTML = "video #" + j
-        document.getElementById("parent" + i).appendChild(button)
+        prev_btn.innerHTML = "Previous"
+        btn_div.appendChild(prev_btn)
+        next_btn = document.createElement("button")
+        next_btn.id = i
+        next_btn.onclick = event => {
+            next(event.target.id)
+        }
+        next_btn.innerHTML = "Next"
+        btn_div.appendChild(next_btn)
+        btn_sec = document.createElement("section")
+        btn_sec.id = "parent" + i
+        btn_sec.style = "text-align:center"
+        master.appendChild(btn_sec)
+        for (var j = 0; j < videos[i].length; j++) {
+            //create stuff there is multiple of
+            button = document.createElement("button")
+            button.id = "button" + i + j
+            button.onclick = event => {
+                var player_id = event.target.id.substring(6, 7)
+                var index = event.target.id.substring(7)
+                document.getElementById("player" + player_id).src = videos[player_id][index]
+                current[i] = index
+            }
+            button.innerHTML = "video #" + j
+            btn_sec.appendChild(button)
+        }
     }
 }
+gen_page()
