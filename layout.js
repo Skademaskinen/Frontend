d8aa2bb1ff318fa71660dc315708d69f0f03b772
Mast3rwaf1z <thomasmollerjensen@live.dk>
2022-04-05 23:22:10 +0200

    added consistent design

---
+++ layout.js
@@ -0,0 +1,65 @@
+function header() {
+    master = document.getElementById("header")
+    div = document.createElement("div")
+    index = document.createElement("a")
+    repo = document.createElement("a")
+    skademaskinen = document.createElement("a")
+    moerbot = document.createElement("a")
+    shitgame = document.createElement("a")
+    rp = document.createElement("a")
+    gaming = document.createElement("a")
+    contact = document.createElement("a")
+    hr = document.createElement("hr")
+    div.id = "div"
+    index.href = "index.html"
+    index.innerHTML = "Home"
+    repo.href = "https://github.com/Mast3rwaf1z/Mast3rwaf1z.github.io"
+    repo.innerHTML = "Source"
+    skademaskinen.href = "https://i.skademaskinen.win"
+    skademaskinen.innerHTML = "Skademaskinen"
+    moerbot.href = "https://github.com/Mast3rwaf1z/Moer-bot"
+    moerbot.innerHTML = "Mørbot"
+    shitgame.href = "https://github.com/Mast3rwaf1z/scrap"
+    shitgame.innerHTML = "Shitgame"
+    rp.href = "https://github.com/Mast3rwaf1z/rp"
+    rp.innerHTML = "RP"
+    gaming.href = "games.html"
+    gaming.innerHTML = "Gaming"
+    contact.href = "contact.html"
+    contact.innerHTML = "Contact"
+    div.appendChild(index)
+    div.appendChild(document.createTextNode(" | "))
+    div.appendChild(repo)
+    div.appendChild(document.createTextNode(" | "))
+    div.appendChild(skademaskinen)
+    div.appendChild(document.createTextNode(" | "))
+    div.appendChild(moerbot)
+    div.appendChild(document.createTextNode(" | "))
+    div.appendChild(shitgame)
+    div.appendChild(document.createTextNode(" | "))
+    div.appendChild(rp)
+    div.appendChild(document.createTextNode(" | "))
+    div.appendChild(gaming)
+    div.appendChild(document.createTextNode(" | "))
+    div.appendChild(contact)
+    div.appendChild(hr)
+    master.appendChild(div)
+    divE = document.querySelector("div")
+    divE.classList.add("center")
+}
+
+function footer() {
+    master = document.getElementById("footer")
+    div = document.createElement("div")
+    hr = document.createElement("hr")
+    txt = document.createTextNode("Please note that this website is entirely for fun and should not be taken seriously unless its obvious.")
+    div.style = "color: gray; background-color: black;"
+    div.appendChild(hr)
+    div.appendChild(txt)
+    master.appendChild(div)
+}
+
+
+
+header()
+footer()
