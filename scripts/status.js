9c4cb6cd2448bfbf7fb60ef2e6728d01ad36805a
Mast3r_waf1z <thomasmollerjensen@live.dk>
2024-02-04 03:23:09 +0100

    admin update

--- scripts/status.js
+++ scripts/status.js
@@ -1,25 +1,24 @@
 var token = getCookie("accessToken")
-if(token == ""){
-    return
+if(token != ""){
+    
+    fetch("https://skademaskinen.win:11034/admin/status?id="+document.getElementById("id")+"&token="+token, {
+        method:"get"
+    }).then(response => {
+        switch(response.status){
+            case 200:
+                response.text.then(text => {
+                    var data = JSON.parse(text)
+                    var systemctl = document.getElementById("systemctl")
+                    systemctl.innerHTML = data["systemctl"]
+                    var update = document.getElementById("update")
+                    update.innerHTML = data["update"]
+                    var lsblk = document.getElementById("lsblk")
+                    lsblk.innerHTML = data["lsblk"]
+                    var errors = document.getElementById("errors")
+                    errors.innerHTML = data["errors"]
+                })
+                default:
+                    console.log("Error!")
+        }
+    })
 }
-
-fetch("https://skademaskinen.win:11034/admin/status?id="+document.getElementById("id")+"&token="+token, {
-    method:"get"
-}).then(response => {
-    switch(response.status){
-        case 200:
-            response.text.then(text => {
-                var data = JSON.parse(text)
-                var systemctl = document.getElementById("systemctl")
-                systemctl.innerHTML = data["systemctl"]
-                var update = document.getElementById("update")
-                update.innerHTML = data["update"]
-                var lsblk = document.getElementById("lsblk")
-                lsblk.innerHTML = data["lsblk"]
-                var errors = document.getElementById("errors")
-                errors.innerHTML = data["errors"]
-            })
-        default:
-            console.log("Error!")
-    }
-})
