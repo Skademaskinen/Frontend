c707d6c02ade33fa5c1961b0906e4bcdc3f2e187
Mast3r_waf1z <thomasmollerjensen@live.dk>
2024-02-06 23:18:04 +0100

    added path to delete function

--- scripts/common.js
+++ scripts/common.js
@@ -16,7 +16,7 @@ function getCookie(cname) {
 }
 
 function deleteCookie(name){
-    document.cookie = name + "=" + getCookie(name)+";max-age=0;expires="+(new Date(0))
+    document.cookie = name + "=" + getCookie(name)+";path=/;max-age=0;"
 }
 
 
