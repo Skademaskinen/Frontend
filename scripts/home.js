d915ce2c8f830a6a5707d843f7e2507218def9c5
Mast3r_waf1z <thomasmollerjensen@live.dk>
2024-02-06 12:54:39 +0100

    implemented new formatting for boot devices

--- scripts/home.js
+++ scripts/home.js
@@ -1,3 +1,6 @@
+var modal = document.getElementById("modal")
+var closeButton = 
+
 async function bootDevice(mac){
     fetch((await getHome())+"/boot", {
         method: "post",
@@ -15,16 +18,52 @@ async function generateDeviceButtons(){
         method: "get"
     }).then(response => response.json().then(devices => {
         devices.forEach(device => {
-            var deviceDiv = document.getElementById("devices")
+            var devicesDiv = document.getElementById("devices")
+            devicesDiv.className = "devices"
+            var deviceDiv = document.createElement("div")
+            deviceDiv.className = "device"
             var button = document.createElement("button")
             button.id = "device-" + device.mac
-            if(!(device.alias == null)){
-                button.innerHTML = device.alias
+            button.innerHTML = "Boot"
+            button.className = "device-button"
+            var setAlias = document.createElement("button")
+            setAlias.innerHTML = "Set Name"
+            setAlias.onclick = () => {
+                if(modal.style.display == "block"){
+                    modal.style.display = "none"
+                    console.log("closed modal")
+                }
+                else{
+                    modal.style.display = "block"
+                    console.log("opened modal")
+                    var mac = document.getElementById("mac")
+                    mac.innerHTML = device.mac
+                    var confirmButton = document.getElementById("set-name-confirm")
+                    confirmButton.onclick = async () => {
+                        modal.style.display = "none"
+                        fetch((await getHome())+"/setalias", {
+                            method:"post",
+                            body:JSON.stringify({
+                                token:getCookie("accessToken"),
+                                mac:device.mac,
+                                alias:document.getElementById("set-name-input").value
+                            })
+                        }).then(_ => {
+                            document.getElementById("set-name-input").value = ""
+                            window.location.reload()
+                        })
+                    }
+                }
             }
-            else {
-                button.innerHTML = "device #" + device.mac
-            }
+            var aliasH = document.createElement("h3")
+            aliasH.innerHTML = device.alias
+            var macP = document.createElement("p")
+            macP.innerHTML = "Mac Address: " + device.mac
+            deviceDiv.appendChild(aliasH)
+            deviceDiv.appendChild(macP)
             deviceDiv.appendChild(button)
+            deviceDiv.appendChild(setAlias)
+            devicesDiv.appendChild(deviceDiv)
             button.onclick = () => bootDevice(device.mac)
         })
     }))
